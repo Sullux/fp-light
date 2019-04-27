5c77ba3da87a3a49e1864c3b6d8d69ad1e5b95f0
Charles Sullivan <sullux@gmail.com>
2019-04-27 18:32:18 +0000

    add the resolve utility

---
+++ lib/resolve/README.md
@@ -0,0 +1,17 @@
+[home](https://github.com/Sullux/fp-light/blob/master/README.md)
+
+## fp-light-resolve
+
+`npm i @sullux/fp-light-resolve`  
+[source](https://github.com/Sullux/fp-light/blob/master/lib/resolve/resolve.js)  
+[test](https://github.com/Sullux/fp-light/blob/master/lib/resolve/resolve.spec.js)
+
+Deeply resolves all functions and promises.
+
+* [resolve](#resolve)
+
+### resolve
+
+`resolve(any)`
+
+todo...
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,52 @@
 const { join } = require('path')
+const { DefinePlugin } = require('webpack')
+
+const docsCollection = []
+
+const featureFlags = new DefinePlugin({
+  __TEST__: !!process.env.TEST,
+  __DOCS__: docs => docsCollection.push(docs),
+  // $test: () => {},
+  // $spec: () => {},
+})
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './index.js',
+  devtool: 'source-map',
   output: {
     path: join(__dirname, 'dist'),
     filename: 'index.js',
     libraryTarget: 'commonjs2',
   },
   target: 'node',
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/,
+  //       use: [
+  //         { loader: './test-loader.js' },
+  //       ],
+  //     },
+  //   ],
+  // },
+  plugins: [
+    featureFlags,
+    // {
+    //   apply: compiler =>
+    //     compiler.hooks.compilation.tap('compilationPlugin', (compilation, compilationParams) => {
+    //       console.log('=== compilation ===')
+    //       console.log(Object.keys(compilation))
+    //       // console.log('=== compilation params ===') ||
+    //       // console.log(compilationParams)
+    //       console.log('=== modules ===')
+    //       console.log(compilation.modules)
+    //       const data = stringify(docsCollection)
+    //       compilation.assets['docs.md'] = {
+    //         source: () => data,
+    //         size: () => data.length,
+    //       }
+    //     }),
+    // },
+  ],
 }
