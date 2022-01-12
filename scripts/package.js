221590cfc7d51831a869f387d33a6a92a4a5a520
Charles Sullivan <charles.sullivan@jetclosing.com>
2022-01-12 12:28:04 -0400

    compile cjs with rollup and remove autodoc comments

--- scripts/package.js
+++ scripts/package.js
@@ -3,4 +3,5 @@ import { readFileSync, writeFileSync } from 'fs'
 const packageJson = JSON.parse(readFileSync('./package.json'))
 delete packageJson.scripts
 delete packageJson.devDependencies
+delete packageJson.type
 writeFileSync('./dist/package.json', JSON.stringify(packageJson, null, 2))
