729fff72a143844a14e86bf197c8f1f6f1e3ea43
Charles Sullivan <sullux@gmail.com>
2022-12-05 08:08:49 -0400

    start first pass interface

--- scripts/package.js
+++
@@ -1,7 +0,0 @@
-import { readFileSync, writeFileSync } from 'fs'
-
-const packageJson = JSON.parse(readFileSync('./package.json'))
-delete packageJson.scripts
-delete packageJson.devDependencies
-delete packageJson.type
-writeFileSync('./dist/package.json', JSON.stringify(packageJson, null, 2))
